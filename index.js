14d01ad0423816ddab829506708e522157fbf8a6
pratheek k <pratheekkmurthy516@gmail.com>
2020-05-18 14:40:59 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -19,7 +19,6 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res) => {
-  res.send({ hi: 'Buddy'});
-});
-
+  res.send({ hi: 'there'});
+const PORT =process.env.PORT || 5000;
   app.listen(5000);
