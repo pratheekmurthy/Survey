71a300381c38d23204a9ff5e85382e4c9bc98d02
pratheek k <pratheekkmurthy516@gmail.com>
2020-06-21 20:52:58 +0530

    installing mongoose

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require("passport");
+const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 
+const User = mongoose.model("users");
 passport.use(
   new GoogleStrategy(
     {
@@ -10,9 +12,7 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile:", profile);
+      new User({ googleID: profile.id }).save();
     }
   )
 ); // Here we are paasing two arguments to google strategy one is credentials and second one is acesstoken
